f1320797b8e13c0700688cabcb4b37208babc924
fransisnk <fmkolsto@student.matnat.uio.no>
2018-09-04 07:50:50 +0200

    change to monkeypatch, fix return value

--- draw_template.py
+++ draw_template.py
@@ -1,30 +1,7 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import (absolute_import, division, print_function)
-
-from branca.element import CssLink, Figure, JavascriptLink, MacroElement
-
 from jinja2 import Template
 
 
-class Draw(MacroElement):
-    """
-    Vector drawing and editing plugin for Leaflet.
-
-    Examples
-    --------
-    >>> m = folium.Map()
-    >>> Draw().draw.add_to(m)
-
-    For more info please check
-    https://leaflet.github.io/Leaflet.draw/docs/leaflet-draw-latest.html
-
-    """
-    def __init__(self):
-        super(Draw, self).__init__()
-        self._name = 'DrawControl'
-
-        self._template = Template(u"""
+_template = Template(u"""
             {% macro script(this, kwargs) %}
             // FeatureGroup is to store editable layers.
             var drawnItems = new L.featureGroup().addTo({{this._parent.get_name()}});
@@ -73,16 +50,3 @@ class Draw(MacroElement):
              });
             {% endmacro %}
             """)
-
-    def render(self, **kwargs):
-        super(Draw, self).render()
-
-        figure = self.get_root()
-        assert isinstance(figure, Figure), ('You cannot render this Element '
-                                            'if it is not in a Figure.')
-
-        figure.header.add_child(
-            JavascriptLink('https://cdn.rawgit.com/Leaflet/Leaflet.draw/v0.4.12/dist/leaflet.draw.js'))  # noqa
-
-        figure.header.add_child(
-            CssLink('https://cdn.rawgit.com/Leaflet/Leaflet.draw/v0.4.12/dist/leaflet.draw.css'))  # noqa
--- webapp.py
+++ webapp.py
@@ -1,38 +1,28 @@
 from flask import Flask, render_template, request
-from draw import Draw
 import folium
-import sys
-from flask_restful  import Resource, Api
-import sys
+from folium.plugins import Draw
+from flask_restful  import Api
 import json
-
+from draw_template import _template
 app = Flask(__name__)
 api = Api(app)
 
+
 @app.route("/")
 def mappy():
+    center = [65, 11]
+    m = folium.Map(location=center, zoom_start=5)
+    Draw._template = _template
+    Draw().add_to(m)
+    m.save("templates/map.html")
+    return render_template("index.html")
 
-	center = [65,11]
-	m = folium.Map(location = center, zoom_start = 5)
-	Draw().add_to(m)
-	m.save("templates/map.html")
-	return render_template("index.html")
 
-@app.route('/get_routes', methods = ['POST'])
+@app.route('/get_routes', methods=['POST'])
 def get_routes():
     jsdata = request.form['boundaries']
-
-    return json.dumps([{
-                      "type": "LineString",
-                      "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-                  }, {
-                      "type": "LineString",
-                      "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-                  }])
-
-
-
+    return json.dumps(jsdata)
 
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
